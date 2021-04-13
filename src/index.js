b36ada2538c755b5e4ef9270164738f17b741a0e
Prakhar Agrawal <fzd.prakhar@gmail.com>
2021-04-13 23:08:52 +0530

    changes updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const firebaseConfig = {
+  apiKey: "AIzaSyBaiiI2sVzAbGH3xJiCVudEbSeZIoOaGjY",
+  authDomain: "cart-90b3a.firebaseapp.com",
+  projectId: "cart-90b3a",
+  storageBucket: "cart-90b3a.appspot.com",
+  messagingSenderId: "924834427665",
+  appId: "1:924834427665:web:ea28481c7f738ba45a92b0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
